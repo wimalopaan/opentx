84c03822262ae937045751a98f5160f8f4990642
Janek <jan.kozak@outlook.com>
2021-10-21 12:23:57 +0200

    - skip on trimms flood

--- radio/src/targets/flysky/buzzer_driver.cpp
+++ radio/src/targets/flysky/buzzer_driver.cpp
@@ -281,7 +281,7 @@ void playTone(uint16_t freq, uint16_t len, uint16_t pause, uint8_t flags, int8_t
       if (!buzzerFifo.full())
         buzzerFifo.push(BuzzerTone(freq, len, pause, flags, freqIncr));
       return;
-  } else if ((flags & PLAY_NOW) && (buzzerState.duration > 40 || buzzerState.repeat > 0)) { // push back to queue
+  } else if ((flags & PLAY_NOW) && (buzzerState.repeat > 0)) { // push back to queue
       if (!buzzerFifo.full()) {
         buzzerFifo.push(BuzzerTone(
           buzzerState.tone.freq,
