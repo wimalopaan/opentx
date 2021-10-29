30086f9ce24a211ee8ed9ea535e8faac64294911
Janek <jan.kozak@outlook.com>
2021-10-29 20:20:50 +0200

    After reading this one looks like more suitable.

--- radio/src/targets/common/arm/stm32/usb_bsp.c
+++ radio/src/targets/common/arm/stm32/usb_bsp.c
@@ -63,7 +63,7 @@ void USB_BSP_Init(USB_CORE_HANDLE *pdev) {
   GPIO_InitStructure.GPIO_Pin = USB_GPIO_PIN_VBUS;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
   GPIO_Init(USB_GPIO, &GPIO_InitStructure);
 }
