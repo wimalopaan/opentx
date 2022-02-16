3292f664c1f7045e69eaca67de6f16ac4f2f04b4
janek <janek.janek@gmail.com>
2022-02-16 19:52:56 +0100

    Move adcRead() to mixer scheduler IRQ to reduce sample jitter #8480 backport

--- radio/src/targets/common/arm/stm32/mixer_scheduler_driver.cpp
+++ radio/src/targets/common/arm/stm32/mixer_scheduler_driver.cpp
@@ -76,6 +76,11 @@ extern "C" void MIXER_SCHEDULER_TIMER_IRQHandler(void)
 
   // set next period
   MIXER_SCHEDULER_TIMER->ARR = getMixerSchedulerPeriod() - 1;
+
+  //read ADCs 
+  DEBUG_TIMER_START(debugTimerAdcRead);
+  adcRead();
+  DEBUG_TIMER_STOP(debugTimerAdcRead);
  
   // trigger mixer start
   mixerSchedulerISRTrigger();
