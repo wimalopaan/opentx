65e81f6820fab1a935a738dc01249648e0535bc5
janek <janek.janek@gmail.com>
2022-11-01 21:48:00 +0100

    Disabled PT translation due to size restrictions. Remove asterisk icon for elrsv3 build for CZ language due to size restrictions.

--- radio/src/gui/128x64/popups.cpp
+++ radio/src/gui/128x64/popups.cpp
@@ -51,12 +51,15 @@ const unsigned char ASTERISK_BITMAP[]  = {
 void drawAlertBox(const char * title, const char * text, const char * action)
 {
   lcdClear();
+
+#if defined(PCBI6X_ELRSV3)
 #if defined(TRANSLATIONS_PT) || defined(TRANSLATIONS_CZ) // not enough flash space
   lcdDrawRect(2, 2, 32 - 4, 32 - 4);
   lcdDrawText(11, 6, "x", DBLSIZE);
 #else
   lcdDraw1bitBitmap(2, 0, ASTERISK_BITMAP, 0, 0);
 #endif
+#endif // PCBI6X_ELRSV3
 
 #define MESSAGE_LCD_OFFSET   6*FW
 
