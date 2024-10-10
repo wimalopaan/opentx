dd06512823afa8b7cefb112ce2808313cacbbfd1
janek <janek.janek@gmail.com>
2024-10-10 21:50:05 +0200

    statistics: removed additional margin

--- radio/src/gui/128x64/view_statistics.cpp
+++ radio/src/gui/128x64/view_statistics.cpp
@@ -239,10 +239,10 @@ void menuStatisticsDebug(event_t event)
   lcdDrawTextAlignedLeft(MENU_DEBUG_Y_RTOS, STR_FREESTACKMINB);
   lcdDrawNumber(MENU_DEBUG_COL1_OFS, MENU_DEBUG_Y_RTOS, menusStack.available(), LEFT);
   lcdDrawText(lcdLastRightPos, MENU_DEBUG_Y_RTOS, "/");
-  lcdDrawNumber(lcdLastRightPos+1, MENU_DEBUG_Y_RTOS, mixerStack.available(), LEFT);
+  lcdDrawNumber(lcdLastRightPos, MENU_DEBUG_Y_RTOS, mixerStack.available(), LEFT);
 #if defined(AUDIO)
   lcdDrawText(lcdLastRightPos, MENU_DEBUG_Y_RTOS, "/");
-  lcdDrawNumber(lcdLastRightPos+1, MENU_DEBUG_Y_RTOS, audioStack.available(), LEFT);
+  lcdDrawNumber(lcdLastRightPos, MENU_DEBUG_Y_RTOS, audioStack.available(), LEFT);
 #endif
 
   lcdDrawText(4*FW, 7*FH+1, STR_MENUTORESET);
