30d6006110a34afb0218d95a9e3b07749dc9e52c
janek <janek.janek@gmail.com>
2024-10-11 15:43:02 +0200

    cleanup

--- radio/src/targets/flysky/board.cpp
+++ radio/src/targets/flysky/board.cpp
@@ -30,75 +30,6 @@ extern "C"
 }
 #endif
 
-extern "C"
-{
-  __attribute__((naked)) __attribute__((used)) void HardFault_HandlerAsm(void)
-  {
-
-    __asm(".syntax unified\n"
-          "MOVS R0, #4 \n"
-          "MOV R1, LR \n"
-          "TST R0, R1 \n"
-          "BEQ _MSP \n"
-          "MRS R0, PSP \n"
-          "B HardFault_HandlerC \n"
-          "_MSP: \n"
-          "MRS R0, MSP \n"
-          "B HardFault_HandlerC \n"
-          ".syntax divided\n");
-  }
-
-  __attribute__((used)) void HardFault_HandlerC(unsigned long *hardfault_args)
-  {
-    /*
-  volatile unsigned long stacked_r0 ;
-  volatile unsigned long stacked_r1 ;
-  volatile unsigned long stacked_r2 ;
-  volatile unsigned long stacked_r3 ;
-  volatile unsigned long stacked_r12 ;
-  volatile unsigned long stacked_lr ;
-  volatile unsigned long stacked_pc ;
-  volatile unsigned long stacked_psr ;
-  volatile unsigned long _CFSR ;
-  volatile unsigned long _HFSR ;
-  volatile unsigned long _DFSR ;
-  volatile unsigned long _AFSR ;
-  volatile unsigned long _BFAR ;
-  volatile unsigned long _MMAR ;
-
-  stacked_r0 = ((unsigned long)hardfault_args[0]) ;
-  stacked_r1 = ((unsigned long)hardfault_args[1]) ;
-  stacked_r2 = ((unsigned long)hardfault_args[2]) ;
-  stacked_r3 = ((unsigned long)hardfault_args[3]) ;
-  stacked_r12 = ((unsigned long)hardfault_args[4]) ;
-  stacked_lr = ((unsigned long)hardfault_args[5]) ;
-  stacked_pc = ((unsigned long)hardfault_args[6]) ;
-  stacked_psr = ((unsigned long)hardfault_args[7]) ;
-
-  // Configurable Fault Status Register
-  // Consists of MMSR, BFSR and UFSR
-  _CFSR = (*((volatile unsigned long *)(0xE000ED28))) ;
-
-  // Hard Fault Status Register
-  _HFSR = (*((volatile unsigned long *)(0xE000ED2C))) ;
-
-  // Debug Fault Status Register
-  _DFSR = (*((volatile unsigned long *)(0xE000ED30))) ;
-
-  // Auxiliary Fault Status Register
-  _AFSR = (*((volatile unsigned long *)(0xE000ED3C))) ;
-
-  // Read the Fault Address Registers. These may not contain valid values.
-  // Check BFARVALID/MMARVALID to see if they are valid values
-  // MemManage Fault Address Register
-  _MMAR = (*((volatile unsigned long *)(0xE000ED34))) ;
-  // Bus Fault Address Register
-  _BFAR = (*((volatile unsigned long *)(0xE000ED38))) ;
-*/
-    __asm("BKPT #0\n"); // Break into the debugger
-  }
-}
-
 #if defined(STM32F0) && defined(BOOT)
 volatile uint32_t __attribute__((section(".ram_vector,\"aw\",%nobits @"))) ram_vector[VECTOR_TABLE_SIZE];
 extern volatile uint32_t g_pfnVectors[VECTOR_TABLE_SIZE];
@@ -229,11 +160,6 @@ void boardInit()
   buzzerInit();
   i2cInit();
   usbInit();
-
-  //storageEraseAll(false);
-  //TRACE("i2c test");
-  //i2c_test();
-  //while (true) ;
 
 #if defined(DEBUG)
   DBGMCU_APB1PeriphConfig(DBGMCU_IWDG_STOP | DBGMCU_TIM1_STOP | DBGMCU_TIM2_STOP | DBGMCU_TIM3_STOP | DBGMCU_TIM6_STOP | DBGMCU_TIM14_STOP, ENABLE);
