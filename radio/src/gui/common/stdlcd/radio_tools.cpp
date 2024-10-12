a533acf529c4eeef3e4f6018b705755c351dba0c
janek <janek.janek@gmail.com>
2024-10-12 14:38:51 +0200

    Remove index column from Radio tools

--- radio/src/gui/common/stdlcd/radio_tools.cpp
+++ radio/src/gui/common/stdlcd/radio_tools.cpp
@@ -26,31 +26,29 @@ void addRadioTool(uint8_t index, const char * label, void (* tool)(event_t event
   int8_t sub = menuVerticalPosition - HEADER_LINE;
   LcdFlags attr = (sub == index ? INVERS : 0);
   coord_t y = MENU_HEADER_HEIGHT + 1 + index * FH;
-  lcdDrawNumber(3, y, index + 1, LEADING0|LEFT, 2);
-  lcdDrawText(3*FW, y, label, (sub == index ? INVERS  : 0));
+  lcdDrawText(0, y, label, attr);
   if (attr && s_editMode > 0) {
     s_editMode = 0;
     pushMenu(tool);
   }
 }
 
-void elrsRun(event_t event);
-void hello_run(event_t event);
-void inavRun(event_t event);
-
 void menuRadioTools(event_t event)
 {
   uint8_t index = 0;
 
 #if defined(PCBI6X_ELRS)
+  extern void elrsRun(event_t event); 
   addRadioTool(index++, "ExpressLRS", elrsRun, event);
 #endif
 
 #if defined(PCBI6X_HELLO)
+  extern void hello_run(event_t event);
   addRadioTool(index++, "Hello World", hello_run, event);
 #endif
 
 #if defined(PCBI6X_INAV)
+  extern void inavRun(event_t event);
   addRadioTool(index++, "INAV Lite", inavRun, event);
 #endif
 
